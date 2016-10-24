71cc9e8aa64c8870f39b57d33085a5c2dd6c4365
Juha Siltanen <juha.siltanen@affecto.com>
2016-10-24 13:09:57 +0300

    Unnecessary logging removed

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -5,7 +5,6 @@ angular.module('palauteUiApp')
 	.constant("baseURL","http://localhost:1234/")
 	.service('feedbackFactory', ['$resource', 'baseURL', function($resource,baseURL) {
 		this.getFeedbacks = function() {
-			console.log("fetching feedbacks resource");
 			return $resource(baseURL+"users/:id",null,  {'update':{method:'PUT' }});
 		};
 	}])
